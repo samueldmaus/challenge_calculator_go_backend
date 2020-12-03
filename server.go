4428dcb90fb3d772d131cf0b1e0d19eb3d575c32
Sam Maus <samueldmaus@gmail.com>
2020-12-02 22:16:56 -0600

    connecting to db

--- server.go
+++ server.go
@@ -8,28 +8,45 @@ import (
 )
 
 const (
-
 	host     = "localhost"
 	port     = 5432
 	user     = "sammaus"
 	dbname   = "calculator"
 )
 
+func sayHi(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"I'm" : "working",
+	})
+}
+
+func getEquations(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"name" : "sam",
+	})
+}
 func main(){
 	var port_num string = "5000"
 	router := gin.New()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"Yes" : "I'm working",
-		})
-	})
+	router.GET("/", sayHi)
 
-	router.GET("/api/equations", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"name" : "sam",
-		})
-	})
+	router.GET("/api/equations", getEquations)
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+    "dbname=%s sslmode=disable",
+    host, port, user, dbname)
+  	db, err := sql.Open("postgres", psqlInfo)
+  	if err != nil {
+    	panic(err)
+  	}
+  	defer db.Close()
+
+  	err = db.Ping()
+  	if err != nil {
+    	panic(err)
+  	}
+
 	router.Run(":" + port_num)
 	fmt.Printf("Listening on port 5000")
 }
