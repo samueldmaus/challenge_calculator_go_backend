d01f5cc9c5f28ac0d3fc5cb757304fd39086c467
Sam Maus <samueldmaus@gmail.com>
2020-12-02 20:54:28 -0600

    added GET request with gin

--- server.go
+++ server.go
@@ -1,14 +1,21 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+	"fmt"
+
 
 	"github.com/gin-gonic/gin"
 )
 
 func main(){
+	var port_num string = "5000"
+	router := gin.New()
 
-	port := 
+	router.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"name" : "sam",
+		})
+	})
+	router.Run(":" + port_num)
+	fmt.Printf("Listening on port 5000")
 }
