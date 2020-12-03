a736b45e847fadcf48cad0a05ddca45af303d7df
Sam Maus <samueldmaus@gmail.com>
2020-12-02 22:52:21 -0600

    getting response back from db

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"database/sql"
+	"github.com/gin-contrib/cors"
 	_ "github.com/lib/pq"
 )
 
@@ -21,18 +22,6 @@ func sayHi(c *gin.Context) {
 }
 
 func getEquations(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"name" : "sam",
-	})
-}
-func main(){
-	var port_num string = "5000"
-	router := gin.New()
-
-	router.GET("/", sayHi)
-
-	router.GET("/api/equations", getEquations)
-
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
     "dbname=%s sslmode=disable",
     host, port, user, dbname)
@@ -46,7 +35,30 @@ func main(){
   	if err != nil {
     	panic(err)
   	}
+	var id int
+	var equation string
+	var answer string
+	sqlStatement := `SELECT * FROM challenge_calculator WHERE id=$1;`
+	row := db.QueryRow(sqlStatement, 3)
+	switch err := row.Scan(&id, &equation, &answer); err {
+	case sql.ErrNoRows:
+  		fmt.Println("No rows were returned!")
+	case nil:
+  		fmt.Println(id, equation, answer)
+	default:
+		  panic(err)
+	}	
+}
 
+func main(){
+	var port_num string = "5000"
+	router := gin.New()
+
+	router.GET("/", sayHi)
+
+	router.GET("/api/equations", getEquations)
+
+	router.Use(cors.Default())
 	router.Run(":" + port_num)
 	fmt.Printf("Listening on port 5000")
 }
