cc110ed570e743e4ed6aa1598726c56e61c354d8
Sam Maus <samueldmaus@gmail.com>
2020-12-02 21:21:12 -0600

    setting up connection to local db

--- server.go
+++ server.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"fmt"
-
-
 	"github.com/gin-gonic/gin"
+	"database/sql"
+	_ "github.com/lib/pq"
+)
+
+const (
+
+	host     = "localhost"
+	port     = 5432
+	user     = "sammaus"
+	dbname   = "calculator"
 )
 
 func main(){
 	var port_num string = "5000"
 	router := gin.New()
 
-	router.GET("/hello", func(c *gin.Context) {
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"Yes" : "I'm working",
+		})
+	})
+
+	router.GET("/api/equations", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"name" : "sam",
 		})
